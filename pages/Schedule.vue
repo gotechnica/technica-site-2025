076f69ca607d74ac6732294ceda041d5a0b9889d
caramurphyy <murphylcara@gmail.com>
2024-06-30 13:03:10 -0700

    copied from 2023

---
+++ pages/Schedule.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="schedule">
+        <Schedule />
+    </div>
+</template>
+
+<style lang="scss">
+.schedule {
+    height: 100vh;
+    padding: 1.5rem 0;
+    background-color: $CREAM;
+    overflow: hidden;
+}
+</style>
