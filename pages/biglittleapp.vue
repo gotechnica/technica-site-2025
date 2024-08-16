3b14d25e6a04902917eaa44637f549b0a2f3cf85
Valentina Hong <valentina.m.hong@gmail.com>
2024-08-15 21:26:21 -0400

    big little application shortlink

---
+++ pages/biglittleapp.vue
@@ -0,0 +1,15 @@
+<template>
+    <meta http-equiv="Refresh" content="0; url=https://docs.google.com/forms/d/e/1FAIpQLSf2dUpdS7uxIA_iDfdbYjEmR6hVAL5qHW5jpmx67M04LSsHuQ/viewform?usp=pp_url" />
+    <h1>Redirecting!!!</h1>
+</template>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        padding: 3rem;
+        color: white;
+    }
+</style>
+
+<script>
+</script>
