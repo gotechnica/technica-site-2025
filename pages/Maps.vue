3f918b6099eb47030391e62e37668e80ae5fe52a
achu1911 <alexacc1911@gmail.com>
2024-10-05 19:41:13 -0400

    Updating maps

--- pages/Maps.vue
+++ pages/Maps.vue
@@ -1,31 +1,54 @@
 <template>
-    <div style="margin-top: 5rem;"></div>
-    <Header>Maps</Header>
-    <div style="margin-top: 5rem;"></div>
+  <title>Maps</title>
     <div class="container">
-      <div class="col">
-        <img class="map" src="/maps/" />
+      <div class="side-by-side">
+        <div class="col">
+          <img class="map" src="/maps/overall_map-01.svg" />
+        </div>
+        <div class="col">
+          <img class="map" src="/maps/tmh_map-02.svg" />
+        </div>
       </div>
-  
-      <div class="col">
-        <img class="map" src="/maps/" />
+      <div class="full-width">
+        <img class="map" src="/maps/arm_main-03.svg" />
       </div>
-      <StickyButton></StickyButton>
+      <div class="full-width">
+        <img class="map" src="/maps/arm_basement-04.svg" />
+      </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  
-  </script>
-  
-  <style scoped lang="scss">
-  .map {
-    max-width: 90vw;
-    margin-top: 3rem;
-  }
-  
-  .container {
-    align-items: center;
-    margin-bottom: 10rem;
-  }
-  </style>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.map {
+  max-width: 100vw;
+  margin-top: 2rem;
+  height: auto;
+}
+
+.side-by-side {
+  display: flex;
+  justify-content: center;
+  gap: 0;
+}
+
+.full-width{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  align-items: center;
+  margin-bottom: 5rem;
+}
+
+.col{
+  display: flex;
+  justify-content: center;
+}
+
+</style>
