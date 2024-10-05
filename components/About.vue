0eeb8d4ba3e605cb90eee6b1d6d8258aad314fd2
Matthew <matthew.yu904@gmail.com>
2024-10-05 13:40:22 -0400

    fix typo compromised -> comprised

--- components/About.vue
+++ components/About.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-6 padding">
                 <p>Technica is the world's largest hackathon for underrepresented genders in tech. The hackathon is hosted annually at the University of Maryland, and is a weekend jam-packed with swag, workshops, networking, and awesome projects!</p>
-                <p>Last year we had over 800+ hackers participate (both in-person and online), compromised of underrepresented groups from many different backgrounds. We had 121 awesome projects submitted, many of which that tackle real-world issues with unique solutions!</p>
+                <p>Last year we had over 800+ hackers participate (both in-person and online), comprised of underrepresented groups from many different backgrounds. We had 121 awesome projects submitted, many of which that tackle real-world issues with unique solutions!</p>
             </div>
             <div class="col-md-6">
                 <div class="video-container">
