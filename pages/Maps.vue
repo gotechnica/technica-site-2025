69184d6c02f5b8ec05dcf8c86aa58fe1e0fc47a7
Serena <pizzalover2317@gmail.com>
2024-10-14 18:29:37 -0400

    missing maps

--- pages/Maps.vue
+++ pages/Maps.vue
@@ -3,17 +3,17 @@
     <div class="container">
       <div class="side-by-side">
         <div class="col">
-          <img class="map" src="/maps/overall_map-01.svg" />
+          <!-- <img class="map" src="/maps/overall_map-01.svg" /> -->
         </div>
         <div class="col">
-          <img class="map" src="/maps/tmh_map-02.svg" />
+          <!-- <img class="map" src="/maps/tmh_map-02.svg" /> -->
         </div>
       </div>
       <div class="full-width">
-        <img class="map" src="/maps/arm_main-03.svg" />
+        <!-- <img class="map" src="/maps/arm_main-03.svg" /> -->
       </div>
       <div class="full-width">
-        <img class="map" src="/maps/arm_basement-04.svg" />
+        <!-- <img class="map" src="/maps/arm_basement-04.svg" /> -->
       </div>
     </div>
 </template>
