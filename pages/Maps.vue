5a9840eace0f484e947286bd22e087bbd6c259b2
achu1911 <alexacc1911@gmail.com>
2024-10-17 00:36:08 -0400

    Fixed format

--- pages/Maps.vue
+++ pages/Maps.vue
@@ -1,21 +1,19 @@
 <template>
   <title>Maps</title>
-    <div class="container">
-      <div class="side-by-side">
-        <div class="col">
-          <img class="map" src="/maps/overall_map-01.svg" />
-        </div>
-        <div class="col">
-          <img class="map" src="/maps/tmh_map-02.svg" />
-        </div>
-      </div>
-      <div class="full-width">
-        <img class="map" src="/maps/arm_main-03.svg" />
-      </div>
-      <div class="full-width">
-        <img class="map" src="/maps/arm_basement-04.svg" />
-      </div>
+  <div class="container">
+    <div class="col">
+      <img class="map map-left" src="/maps/overall_map-01.svg" />
     </div>
+    <div class="col">
+      <img class="map map-right" src="/maps/tmh_map-02.svg" />
+    </div>
+    <div class="full-width">
+      <img class="map" src="/maps/arm_main-03.svg" />
+    </div>
+    <div class="full-width">
+      <img class="map" src="/maps/arm_basement-04.svg" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,30 +23,50 @@
 <style scoped lang="scss">
 .map {
   max-width: 100vw;
-  margin-top: 2rem;
+  margin: -2 auto; 
+  display: block;
   height: auto;
 }
 
-.side-by-side {
-  display: flex;
-  justify-content: center;
-  gap: 0;
+.map-left {
+  transform: translateX(-200px); 
 }
 
-.full-width{
+.map-right {
+  transform: translateX(50px);
+}
+
+.full-width,
+.col {
   width: 100%;
   display: flex;
   justify-content: center;
 }
+.col img {
+  margin-top: 0; 
+  margin-bottom: 0; 
+}
 
 .container {
   align-items: center;
-  margin-bottom: 5rem;
+  margin-bottom: 2rem;
 }
 
-.col{
-  display: flex;
-  justify-content: center;
-}
+@media (max-width: 768px) {
+  .col {
+    flex-direction: column; 
+    align-items: center; 
+    margin-bottom: 1rem;
+  }
 
+  .map-left {
+    transform: translateX(0); 
+    margin: 0; 
+  }
+
+  .map-right {
+    transform: translateX(0);
+    margin: 0; 
+  }
+}
 </style>
