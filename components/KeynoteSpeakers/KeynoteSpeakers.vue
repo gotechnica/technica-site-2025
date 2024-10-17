37c261aadbfd0df80f19db9e759e970c8a2c508a
Kaiwen Song <31936108+Kaidriver@users.noreply.github.com>
2024-10-17 00:35:06 -0400

    update bio + titles for keynote speakers

--- components/KeynoteSpeakers/KeynoteSpeakers.vue
+++ components/KeynoteSpeakers/KeynoteSpeakers.vue
@@ -5,36 +5,39 @@
         <div class="image-wrapper1">
           <img class="speaker-one" src="/keynote-speakers/speaker1.png" />
         </div>
-        <h1 class="speaker-name">Coming Soon!</h1>
-        <p class="speaker-pronouns">Pronouns</p>
-        <div class="dropdown">
+        <h1 class="speaker-name">Eriol Fox</h1>
+        <p class="speaker-info">Senior Human-Rights centered Designer at Superbloom</p>
+        <p class="speaker-info">They/Them</p>
+        <div class="btn-container">
           <button @click="toggleDropdown(1)" class="dropdown-button">
-            <img class="button-image" src="/keynote-speakers/bio-button.png">
+            <PixelButton text="Read Bio" img="purple-button-regular.svg" hover="purple-button-regular.svg" click="purple-button-regular.svg" textAlign="-1.5rem"/>
           </button>
-          <div v-if="isOpen1" class="dropdown-menu">
-            <p>
-              Check back soon for more information!
-            </p>
-          </div>
+          <img class="arrow" :style="{transform: `rotate(${degree1}deg)`}" :src="'/buttons/expand-arrow.svg'" alt="arrow"/>
         </div>
+        
+        <p v-if="isOpen1" class="description">
+          Eriol has been working as a designer for 10+ years working in for-profits and then NGO’s and open-source software organisations, working on complex problems like sustainable food systems, peace-building and crisis response technology. Eriol now works at Superbloom design, research, open-source and technology projects.
+They are also part of the core teams at <a href="http://opensourcedesign.net/">Open Source Design</a> and <a href="https://hrcd.pubpub.org/">Human Rights Centred Design working group</a> and <a href="https://sustainoss.org/working-groups/design-and-ux/">Sustain UX & Design working group</a> and help <a href="https://sosdesign.sustainoss.org/">hosts podcast</a> about open source and design
+Eriol is a non-binary, queer person who uses they/them pronouns.
+        </p>
       </div>
 
       <div class="speaker-card second">
         <div class="image-wrapper2">
           <img class="speaker-one" src="/keynote-speakers/speaker1.png" />
         </div>
-        <h1 class="speaker-name">Coming Soon!</h1>
-        <p class="speaker-pronouns">Pronouns</p>
-        <div class="dropdown">
+        <h1 class="speaker-name">Ayodele Odubela</h1>
+        <p class="speaker-info">Founder, AI ALT Lab</p>
+        <p class="speaker-info">She/Her</p>
+        <div class="btn-container">
           <button @click="toggleDropdown(2)" class="dropdown-button">
-              <img class="button-image" src="/keynote-speakers/bio-button.png">
+            <PixelButton text="Read Bio" img="purple-button-regular.svg" hover="purple-button-regular.svg" click="purple-button-regular.svg" textAlign="-1.5rem"/>
           </button>
-          <div v-if="isOpen2" class="dropdown-menu">
-            <p>
-              Check back soon for more information!
-            </p>
-          </div>
+          <img class="arrow" :style="{transform: `rotate(${degree2}deg)`}" :src="'/buttons/expand-arrow.svg'" alt="arrow"/>
         </div>
+        <p v-if="isOpen2" class="description">
+          Odubela is the Founder of the AI ALT Lab, envisioning alternate approaches to AI that prioritize the well-being of marginalized people. She was previously a data scientist working on auditing HR algorithms, developing ML fairness education at Microsoft, and deploying ML features for various startups. She received her Bachelor’s in Communications from the University of Pittsburgh and went on to receive a Master’s in Data Science from Regis University. In her spare time Ayodele enjoys playing drums and making candles.
+        </p>
       </div>
   </div>
 </template>
@@ -45,12 +48,15 @@ import {ref} from 'vue';
 
 const isOpen1 = ref(false);
 const isOpen2 = ref(false);
-
+let degree1 = 0
+let degree2 = 0
 const toggleDropdown = (dropdownNumber: number) => {
   if (dropdownNumber === 1) {
     isOpen1.value = !isOpen1.value;
+    degree1 = (degree1 == 180 ? 0 : 180)
   } else if (dropdownNumber === 2) {
     isOpen2.value = !isOpen2.value;
+    degree2 = (degree2 == 180 ? 0 : 180)
   }
 };
 </script>
@@ -89,16 +95,6 @@ const toggleDropdown = (dropdownNumber: number) => {
   background-color: transparent;
 
 }
-.dropdown-menu {
-  display: none;
-  position: absolute; 
-  background-color: #66395e;
-  border: 2px #4c083d;
-}
-
-.dropdown-menu {
-  display: block; 
-}
 
 .dropdown-button {
   background-color: transparent;
@@ -107,7 +103,7 @@ const toggleDropdown = (dropdownNumber: number) => {
   cursor: pointer; /* Ensure button is clickable */
 }
 
-.dropdown-menu p {
+.description {
   color: white; 
   text-align: center;
 }
@@ -125,7 +121,7 @@ const toggleDropdown = (dropdownNumber: number) => {
     flex-direction: column;    
     align-items: center; 
     width: 90%;
-    max-width: 250px;
+    max-width: 500px;
     box-sizing: border-box;     
     
   }
@@ -147,8 +143,21 @@ const toggleDropdown = (dropdownNumber: number) => {
     -1.5px 1.5px 0 #00473E;   
 }
 
-  .speaker-pronouns{
+  .speaker-info{
     color: white;
+    text-align: center;
+  }
+
+  .arrow {
+    top: 38%;
+    left: 75%;
+    width: 8%;
+    position: absolute;
+    transition: transform 0.5s ease-in-out;
+  }
+
+  .btn-container {
+    position: relative;
   }
 
   @media screen and (max-width: 480px) {
