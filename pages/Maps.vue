0a4273ba92aaf983485fa8a52c7e1c1415e30a93
Serena <pizzalover2317@gmail.com>
2024-10-20 02:14:04 -0400

    map centering

--- pages/Maps.vue
+++ pages/Maps.vue
@@ -1,13 +1,16 @@
 <template>
   <title>Maps</title>
     <div class="container">
-      <div class="side-by-side">
-        <div class="col">
+      <div class="overall">
+        <div class="title">
+          <Header>Overall Map</Header>
+        </div>
+        <div class="center">
           <img class="map" src="/maps/overall_map-01.svg" />
         </div>
-        <div class="col">
-          <img class="map" src="/maps/tmh_map-02.svg" />
-        </div>
+      </div>
+      <div class="full-width">
+        <img class="map" src="/maps/tmh_map-02.svg" />
       </div>
       <div class="full-width">
         <img class="map" src="/maps/arm_main-03.svg" />
@@ -29,10 +32,12 @@
   height: auto;
 }
 
-.side-by-side {
-  display: flex;
-  justify-content: center;
-  gap: 0;
+.center {
+  margin-right: 25vw;
+}
+
+.title {
+  padding-top: 5rem;
 }
 
 .full-width{
