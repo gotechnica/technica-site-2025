86e855ab611e385b3d5aa8aec222c03b51317b87
Serena <pizzalover2317@gmail.com>
2024-10-24 14:21:08 -0400

    times

--- components/HeroLanding.vue
+++ components/HeroLanding.vue
@@ -12,7 +12,7 @@
 
             <div class="subtext">
                 <h1 class = "outline">Wonder Awaits</h1>
-                <p class = "basic-info outline">October 26-27 at The Armory, UMD | Hybrid Environment</p>
+                <p class = "basic-info outline">9AM October 26 to 5PM October 27th  at The Armory, UMD | Hybrid Environment</p>
                 <p class = "outline">The world's largest hackathon for underrepresented genders</p>
             </div>
             <div id="dummy"></div>
