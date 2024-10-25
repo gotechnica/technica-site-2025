0d904afa41567fd5d0586ae06e9d3194246ce5ec
Serena <pizzalover2317@gmail.com>
2024-10-24 22:14:43 -0400

    added imgs

---
+++ pages/LikeATeaTrayInTheSky.vue
@@ -0,0 +1,15 @@
+<template>
+    <meta http-equiv="Refresh" content="0; url=LikeATeaTrayInTheSky.pdf" />
+    <h1>Redirecting!!!</h1>
+</template>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        padding: 3rem;
+        color: white;
+    }
+</style>
+
+<script>
+</script>
---
+++ pages/painting.vue
@@ -0,0 +1,17 @@
+<template>
+    <meta http-equiv="Refresh" content="0; url=https://forms.gle/XW7hsA9hJhkKj1cFA" />
+
+    <h1>Redirecting!!!</h1>
+</template>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        padding: 3rem;
+        color: white;
+    }
+</style>
+
+<script>
+
+</script>
