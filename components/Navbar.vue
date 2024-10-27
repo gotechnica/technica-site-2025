479dae4ce37f855e266505640ad578a3d74c6e3b
windows <shuang04@terpmail.umd.edu>
2024-10-27 13:43:03 -0400

    capital one

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -66,6 +66,7 @@ export default {
         // { dropdown: false, name: 'Register', path: './Register'},
         /*{ dropdown: false, name: 'Annual Report', path: './Report'},*/
         { dropdown: false, name: 'Expo', path: '/expo' }, 
+        { dropdown: false, name: 'Capital One Judging', path: '/capitalOne'},
         { dropdown: false, name: 'Schedule', path: '/schedule' }, 
         { dropdown: false, name: 'Gather', path: '/gather' }, 
         { dropdown: false, name: 'Maps', path: '/maps' }, 
---
+++ pages/CapitalOne.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="header"><Header>Capital One Judging</Header></div>
+
+    <table>
+        <tr>
+            <th>Team Name</th>
+            <th>Time</th>
+            <th>Location</th>
+        </tr>
+        <tr>
+            <td>John DoeNext Stop: College!</td>
+            <td>2:00</td>
+            <td>Location Unknown</td>
+        </tr>
+        <tr>
+            <td>EasiPeasi Expense Snap</td>
+            <td>2:35</td>
+            <td>Spades B3</td>
+        </tr>
+        <tr>
+            <td>Pennywise</td>
+            <td>2:50</td>
+            <td>Spades C3</td>
+        </tr>
+        <tr>
+            <td>Receiptable</td>
+            <td>3:00</td>
+            <td>Diamonds B1</td>
+        </tr>
+    </table>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+.header{
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+}
+
+table {
+    margin: auto;
+    width: 50%;
+    border-collapse: collapse;
+
+}
+th, td {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+th {
+    background-color: #f2f2f2;
+}
+</style>
