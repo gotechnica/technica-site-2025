c225c83b7c10236db0733d990bd4b6b99cee7da1
Rachel Kim <yseo7045@gmail.com>
2025-05-29 20:53:39 -0400

    Created Profile Cards and added auto-population from json file

---
+++ components/ProfileCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="profile-card">
+    <img
+      :src="image && image.trim() !== '' ? image : placeholder"
+      alt="Profile picture"
+      class="profile-image"
+    />
+    <p>{{ name }}</p>
+    <p>{{ title }}</p>
+    <p>{{ pronouns }}</p>
+  </div>
+</template>
+
+<script setup>
+import blankProfilePic from '../assets/blank-profile-picture.png'
+
+defineProps({
+  name: String,
+  title: String,
+  pronouns: String,
+  image: String
+})
+
+const placeholder = blankProfilePic
+</script>
+
+<style scoped>
+.profile-card {
+  padding: 16px;
+  border-radius: 8px;
+  width: 200px;
+  text-align: center;
+}
+.profile-image {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-bottom: 8px;
+}
+</style>
--- pages/Meet-the-Team.vue
+++ pages/Meet-the-Team.vue
@@ -3,8 +3,16 @@
       <div class="box">
         <Header class="heading">Co-Execs</Header>
         <div class="row">
-          <div class="col name"><h3>Jasmine Emeruwa</h3></div>
-          <div class="col name"><h3>Serena Huang</h3></div>
+            <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in coExecs"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -13,25 +21,16 @@
       <div class="box">
         <Header class="heading">Directors</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Floricel Buencamino</h3>
-            <h3>Tracy Anne Tan</h3>
-            <h3>Faith Comising</h3>
-            <h3>Michelle Yu</h3>
-            <h3>Julie Lin</h3>
-            <h3>Aditi Sethi</h3>
-            <h3>Meenakshi Rama Subramaniane</h3>
-          </div>
-          <div class="col name">
-            <h3>Joanne Lee</h3>
-            <h3>Breanna Pompey</h3>
-            <h3>Victoria Anaele-Nwogu</h3>
-            <h3>Helena Tesfaye</h3>
-            <h3>Claire Chen</h3>
-            <h3>Madeline Moldrem</h3>
-            <h3>Valentina Hong</h3>
-            <h3>Brian Xie</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in directors"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -40,15 +39,15 @@
       <div class="box">
         <Header class="heading">Brand</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Sreya Kanchi</h3>
-            <h3>Habib Camara</h3>
-            <h3>Grace Yang</h3>
-          </div>
-          <div class="col name">
-            <h3>Annie Nguyen</h3>
-            <h3>Trevor Kim</h3>
-            <h3>Zoey Tsai</h3>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in brand"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
           </div>
         </div>
       </div>
@@ -58,18 +57,16 @@
       <div class="box">
         <Header class="heading">Design</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Purva Jani</h3>
-            <h3>Alys Shah</h3>
-            <h3>Mykha Floresca</h3>
-            <h3>Kitty Shi</h3>
-          </div>
-          <div class="col name">
-            <h3>Uyen Nguyen</h3>
-            <h3>Disha Ramesh</h3>
-            <h3>Arielle Bruce</h3>
-            <h3>Alyssa Yen</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in design"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -78,16 +75,16 @@
       <div class="box">
         <Header class="heading">Events</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Maya Chelar</h3>
-            <h3>Avanti Athavale</h3>
-            <h3>Sifene Fufa</h3>
-          </div>
-          <div class="col name">
-            <h3>Sofia Santiago</h3>
-            <h3>Neha Joshi</h3>
-            <h3>Laasya Madduri</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in events"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -96,18 +93,16 @@
       <div class="box">
         <Header class="heading">Experience</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Beatrice Chung</h3>
-            <h3>Dorothy Dang</h3>
-            <h3>Anoushka Arora</h3>
-            <h3>Bhavika Buddi</h3>
-          </div>
-          <div class="col name">
-            <h3>Anannya Trehan</h3>
-            <h3>Sharvari Tirodkar</h3>
-            <h3>Kyle Kuo</h3>
-            <h3>Sheila Li</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in experience"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -116,18 +111,16 @@
       <div class="box">
         <Header class="heading">Inclusive Communities</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Savya Miriyala</h3>
-            <h3>Keya Pawar</h3>
-            <h3>Michelle Akem</h3>
-            <h3>Irene Lee</h3>
-          </div>
-          <div class="col name">
-            <h3>Melat Abera</h3>
-            <h3>Anagha Bhuvanagiri</h3>
-            <h3>Jordan Namie</h3>
-            <h3>Sonaya Dhuria</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in inclusiveCommunities"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -136,19 +129,16 @@
       <div class="box">
         <Header class="heading">Operations</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Safiyah Fatima</h3>
-            <h3>Maggie McAndrew</h3>
-            <h3>Ananya Malipeddi</h3>
-            <h3>Tiffany Liu</h3>
-          </div>
-          <div class="col name">
-            <h3>Alison Wang</h3>
-            <h3>Daanial Haris</h3>
-            <h3>Luke Tan</h3>
-            <h3>Naomi Kaul</h3>
-            <h3>Anshu Chandrasekhar</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in operations"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -157,16 +147,16 @@
       <div class="box">
         <Header class="heading">Outreach</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Samiha Naser</h3>
-            <h3>Keerthana Narabetla</h3>
-            <h3>Sarah King</h3>
-          </div>
-          <div class="col name">
-            <h3>Divya Lingala</h3>
-            <h3>Elizabeth Chung</h3>
-            <h3>Kaavya Radhakrishnan</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in outreach"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -175,17 +165,16 @@
       <div class="box">
         <Header class="heading">Sponsorship</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Rhea Chandragiri</h3>
-            <h3>Anjali Samavedam</h3>
-            <h3>Srinidhi Gubba</h3>
-            <h3>Shree Dave</h3>
-          </div>
-          <div class="col name">
-            <h3>Harleen Green</h3>
-            <h3>Juhi Chitkara</h3>
-            <h3>Aabha Belsare</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in sponsorship"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
@@ -194,20 +183,120 @@
       <div class="box">
         <Header class="heading">Tech</Header>
         <div class="row">
-          <div class="col name">
-            <h3>Sophie Tsai</h3>
-            <h3>Tarun Kommuri</h3>
-            <h3>Annika Wong</h3>
-            <h3>Alexa Chu</h3>
-          </div>
-          <div class="col name">
-            <h3>Rachel Kim</h3>
-            <h3>Meron Abera</h3>
-            <h3>Chaitra Bhumula</h3>
-            <h3>Edward Choi</h3>
-          </div>
+          <div class="card-grid">
+                <ProfileCard
+                v-for="(person, index) in tech"
+                :key="index"
+                :name="person.Name"
+                :title="person.Title"
+                :pronouns="person.Pronouns"
+                :image="person.Image"
+                />
+            </div>
         </div>
       </div>
     </div>
-  </template>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import allProfiles from '../assets/spreadsheets/teams.json'
+import ProfileCard from '../components/ProfileCard.vue'
+
+const coExecs = computed(() =>
+  allProfiles.filter(p => p.Team === 'Co-Execs')
+)
+
+const directors = computed(() =>
+  allProfiles.filter(p => p.Team === 'Directors')
+)
+
+const brand = computed(() =>
+  allProfiles.filter(p => p.Team === 'Brand')
+)
+const design = computed(() =>
+  allProfiles.filter(p => p.Team === 'Design')
+)
+
+const events = computed(() =>
+  allProfiles.filter(p => p.Team === 'Events')
+)
+
+const experience = computed(() =>
+  allProfiles.filter(p => p.Team === 'Experience')
+)
+
+const inclusiveCommunities = computed(() =>
+  allProfiles.filter(p => p.Team === 'Inclusive Communities')
+)
+
+const operations = computed(() =>
+  allProfiles.filter(p => p.Team === 'Operations')
+)
+
+const outreach = computed(() =>
+  allProfiles.filter(p => p.Team === 'Outreach')
+)
+
+const sponsorship = computed(() =>
+  allProfiles.filter(p => p.Team === 'Sponsorship')
+)
+
+const tech = computed(() =>
+  allProfiles.filter(p => p.Team === 'Tech')
+)
+</script>
+
+<style scoped>
+
+.h3 {
+    text-align: center;
+}
+
+.heading {
+  margin-bottom: 2rem;
+}
+
+.box {
+  background-color: rgba(56, 104, 62, 0.2);
+  padding: 2rem;
+  border-radius: 15px;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+}
+
+.row {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    width: 100%;
+}
+
+.col {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.name {
+    color: white;
+}
+
+.card-grid {
+  display: inline-grid;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  gap: 1.5rem;
+  justify-items: center;
+  justify-content: center;
+}
+</style>
   
