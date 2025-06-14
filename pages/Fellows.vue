245113a741fa255852f6344d5040f7665f910c48
achu1911 <alexacc1911@gmail.com>
2025-06-14 19:57:59 -0400

    fixing images

--- pages/Fellows.vue
+++ pages/Fellows.vue
@@ -450,8 +450,10 @@ hr{
 
 .project-card {
   position: relative;
-  background-color: #6CB4AC;
+  background-color: transparent;
   border: 3px solid #000000;
+  width: 100%;
+  height: auto;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -472,10 +474,12 @@ hr{
 }
 
 .project-image {
+  position: absolute;
   width: 100%;
   height: 100%;
   object-fit: cover;
   display: block;
+
 }
 
 .project-card:last-child {
