3105d96188506e685bd6777e8f13c421d600610b
Tarun Kommuri <kommuri.tarun@gmail.com>
2025-06-29 19:48:58 -0400

    Retheming About Technica, Our Achievements, and Keynote Speakers

--- components/About.vue
+++ components/About.vue
@@ -2,24 +2,25 @@
   <div class="wrapper">
     <Header>About Technica</Header>
     <div class="row">
-      <div class="col-md-6 padding">
+      <div class="col-md-6 info-box">
         <p>
-          Technica is the world's largest hackathon for underrepresented genders
-          in tech. The hackathon is hosted annually at the University of
-          Maryland, and is a weekend jam-packed with swag, workshops,
-          networking, and awesome projects!
+          Technica is the world's largest hackathon for underrepresented genders 
+          in tech. The hackathon is hosted annually at the University of Maryland, 
+          and is a weekend jam-packed with swag, workshops, networking, and awesome projects!
+
         </p>
         <p>
-          Last year we had over 800+ hackers participate (both in-person and
-          online), comprised of underrepresented groups from many different
-          backgrounds. We had 114 awesome projects submitted, many of which that
-          tackle real-world issues with unique solutions!
+          Last year we had over 800+ hackers participate (both in-person and online), 
+          comprised of underrepresented groups from many different backgrounds. We 
+          had 121 awesome projects submitted, many of which that tackle real-world 
+          issues with unique solutions!
         </p>
       </div>
       <div class="col-md-6">
-        <div class="video-container">
+        <div class="tv-wrapper">
+          <img src="public\about\intro_vid_container.svg" alt="TV frame" class="tv-frame" />
           <iframe
-            class="recap-video"
+            class="tv-video"
             src="https://www.youtube.com/embed/YZZYDJbuJF8?si=VeMp8j6L7SlOkPoi"
             frameborder="0"
             allowfullscreen
@@ -30,24 +31,58 @@
     </div>
   </div>
 </template>
-<script setup="ts"></script>
-<style scoped lang="scss">
-.video-container iframe {
-  width: 100%;
-  height: 100%;
-}
 
-.video-container {
-  width: 100%;
-  height: 25vw;
-}
+
+<script setup="ts"></script>
+
+<style scoped lang="scss">
+
 
 .col-md-6 {
   margin-top: 3%;
 }
 
-.padding {
-  padding: 5%;
+.tv-wrapper {
+  position: relative;
+  width: 700px; 
+  height: 360px; 
+}
+
+.tv-frame {
+  position: relative;
+  z-index: 0;
+}
+
+.tv-video {
+  position: absolute;
+  top: 16%;
+  left: 12%;
+  width: 76%;
+  height: 68%;
+  z-index: 2; 
+  border-radius: 4px;
+  box-shadow: 0 0 0 9999px #1a1b27; 
+  clip-path: inset(0 round 8px); 
+}
+
+.info-box {
+  background-color: #1a1b27;
+  color: #ffffff;
+  padding: 2rem;
+  border-radius: 16px;
+  border: 2px solid #ffffff;
+
+  box-shadow:
+    // white outer glow
+    0 0 20px rgba(255, 255, 255, 0.4),
+    // purple haze
+    0 0 40px rgba(150, 120, 255, 0.3),
+    // soft blue aura
+    0 0 60px rgba(100, 120, 255, 0.2);
+
+  font-size: 1.1rem;
+  line-height: 1.6;
+  transition: box-shadow 0.3s ease;
 }
 p {
   color: $DARKWHITE;
--- components/KeynoteSpeakers/KeynoteSpeakers.vue
+++ components/KeynoteSpeakers/KeynoteSpeakers.vue
@@ -1,11 +1,12 @@
 <template>
   <Header>Keynote Speakers</Header>
-  <!-- <div class="speaker-container">
+  <div class="speaker-container">
       <div class="speaker-card first">
         <div class="image-wrapper1">
+          <img src="/public/keynote-speakers/speaker_frame_1.svg" alt="ghost frame" />
         </div>
-        <h1 class="speaker-name">Eriol Fox</h1>
-        <p class="speaker-info">Senior Human-Rights centered Designer at Superbloom</p>
+        <h1 class="speaker-name">Name Lastname</h1>
+        <p class="speaker-info">Title</p>
         <p class="speaker-info">They/Them</p>
         <div class="btn-container">
           <button @click="toggleDropdown(1)" class="dropdown-button">
@@ -15,18 +16,17 @@
         </div>
         
         <p v-if="isOpen1" class="description">
-          Eriol has been working as a designer for 10+ years working in for-profits and then NGO’s and open-source software organisations, working on complex problems like sustainable food systems, peace-building and crisis response technology. Eriol now works at Superbloom design, research, open-source and technology projects.
-They are also part of the core teams at <a href="http://opensourcedesign.net/">Open Source Design</a>, <a href="https://hrcd.pubpub.org/">Human Rights Centred Design working group</a>, <a href="https://sustainoss.org/working-groups/design-and-ux/">Sustain UX & Design working group</a>, and help <a href="https://sosdesign.sustainoss.org/">hosts podcast</a> about open source and design.
-Eriol is a non-binary, queer person who uses they/them pronouns.
+          Bio here:
         </p>
       </div>
 
       <div class="speaker-card second">
         <div class="image-wrapper2">
+          <img src="/public/keynote-speakers/speaker_frame_2.svg" alt="ghost frame" />
         </div>
-        <h1 class="speaker-name">Ayodele Odubela</h1>
-        <p class="speaker-info">Founder, AI ALT Lab</p>
-        <p class="speaker-info">She/Her</p>
+        <h1 class="speaker-name">Name Lastname</h1>
+        <p class="speaker-info">Title</p>
+        <p class="speaker-info">They/Them</p>
         <div class="btn-container">
           <button @click="toggleDropdown(2)" class="dropdown-button">
             <WebButton text="Read Bio" img="button_pink_normal.svg" hover="button_pink_hover.svg" click="button_pink_onclick.svg" textAlign="-1.5rem"/>
@@ -34,10 +34,10 @@ Eriol is a non-binary, queer person who uses they/them pronouns.
           <img class="arrow" :style="{transform: `rotate(${degree2}deg)`}" :src="'/buttons/expand-arrow.svg'" alt="arrow"/>
         </div>
         <p v-if="isOpen2" class="description">
-          Odubela is the Founder of the AI ALT Lab, envisioning alternate approaches to AI that prioritize the well-being of marginalized people. She was previously a data scientist working on auditing HR algorithms, developing ML fairness education at Microsoft, and deploying ML features for various startups. She received her Bachelor’s in Communications from the University of Pittsburgh and went on to receive a Master’s in Data Science from Regis University. In her spare time Ayodele enjoys playing drums and making candles.
+          Bio here:
         </p>
       </div>
-  </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
