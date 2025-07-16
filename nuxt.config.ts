fd2d619425fe961031f7f0945152a171d2d9d775
Valentina Hong <valentina.m.hong@gmail.com>
2025-07-16 10:51:03 -0400

    editing config for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/' : '/',
+  },
   typescript: {
     strict: true,
     typeCheck: true,
