1953899fcf815e0240a8544b16c7f1ef1fc9b3b4
BrianXie-03 <Brianxie1298@gmail.com>
2025-07-15 20:11:20 -0500

    change api paths

--- composables/useUtils.ts
+++ composables/useUtils.ts
@@ -31,7 +31,7 @@ export function useUtils(){
             DATES: DATES_ALL
         },
         prd: {
-            BACKEND_ENDPOINT: 'https://1m8yutyf2b.execute-api.us-east-1.amazonaws.com',
+            BACKEND_ENDPOINT: 'https://uq19bixxn1.execute-api.us-east-1.amazonaws.com',
             DATES: DATES_ALL
         },
         bucket: {
