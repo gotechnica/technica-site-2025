c68d9bd35f9034a7874a897a1f59bbfc57ebde7b
BrianXie-03 <Brianxie1298@gmail.com>
2025-07-16 00:13:06 -0500

    lowercase file path

--- pages/Register.vue
+++ pages/Register.vue
@@ -1472,7 +1472,7 @@ const registerUser = async (values: Record<string, any>) => {
     } else if (response.error.value && response.error.value.statusCode == 500) {
       alert('status code 500');
     } else {
-      location.href = '/RegistrationConfirmation';
+      location.href = '/registrationconfirmation';
     }
   } catch (error: any) {
     alert(
