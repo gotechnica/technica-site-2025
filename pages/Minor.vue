1e5942f3925fa931d6ee30d51cde00c5ed85175f
Alexa Chu <alexacc1911@gmail.com>
2025-10-17 06:26:31 -0400

    retheming + updating minors page

--- pages/Minor.vue
+++ pages/Minor.vue
@@ -24,10 +24,10 @@
           Whether participating in person or virtually, a minor’s
           chaperone/guardian must submit Technica’s Minor Waiver for the minor.
           This waiver is linked
-          <a href="../minor/2024 Technica Minors Waiver.pdf" target="_blank">
+          <a href="../minor/2025 Technica Minors Waiver.pdf" target="_blank">
             here</a
           >
-          and will also be emailed to the hacker’s parent/guardian in early
+          and will also be emailed to the hacker’s parent/guardian in late
           October to sign through Docusign (given that the hacker is
           registered). The parent/guardian must also attach scans of valid
           government ID in this Docusign. Only one signed copy of the waiver is
@@ -40,7 +40,7 @@
           Unfortunately, we are unable to provide overnight options for minors
           at Technica this year. Participants under the age of 18 will be
           required to leave Technica premises with their chaperone by 10:00 pm
-          on October 26th and may not return before 8:00 am on October 27th.
+          on November 15th and may not return before 8:00 am on November 16th.
           Minors can continue to participate virtually during these hours if
           they choose. This decision is solely due to liability concerns given
           the size of our event.
@@ -71,12 +71,7 @@ const qaList = [
   {
     question: 'Where and when is Technica?',
     answer:
-      'Technica 2024 will take place on October 26-27 in Reckord Armory and Thurgood Marshall Hall at UMD and virtually via our online platform, Gather. You choose how you want to join! In-person check-in will begin at 9 AM on Saturday and the event will conclude at 5 PM on Sunday.',
-  },
-  {
-    question: 'Can minors ride Technica-chartered buses?',
-    answer:
-      'Minors can ride Technica-chartered or -contracted vehicles, though it is advised that minors are accompanied by a chaperone on these vehicles. Please refer to the Minors Waiver for more information.',
+      'Technica 2025 will take place on November 15-16 in the College Park Marriott Hotel & Conference Center and virtually via our online platform, Gather. You choose how you want to join! In-person check-in will begin at 9 AM on Saturday and the event will conclude at 5 PM on Sunday.',
   },
   {
     question: 'Do chaperones have to register for Technica?',
@@ -89,20 +84,18 @@ const qaList = [
       'A single chaperone is allowed to accompany up to 10 minors, but a separate waiver needs to be completed for each minor in the group. Additionally, the parent/guardian must sign off on the waiver, regardless of whether or not they will be accompanying the minor as a chaperone.',
   },
   {
-    question:
-      'Can minors continue to participate virtually during curfew hours?',
-    answer:
-      'Yes, minors are welcome (and encouraged!) to continue participating virtually on the Gather platform during curfew hours.',
-  },
-  {
     question: 'How do I submit my signed minors waiver?',
     answer:
-      'A Docusign will be emailed to registered hacker’s parent/guardian in early October for signage. A copy of the waiver can be found <a href ="https://drive.google.com/file/u/0/d/1OesK8GFb-V6NWDKceDZ02VNi9kGxKAKP/view?usp=sharing&pli=1" target="_blank">here</a> for early viewing.',
+      'A Docusign will be emailed to registered hacker’s parent/guardian in late October for signage. A copy of the waiver can be found <a href ="https://drive.google.com/file/u/0/d/1OesK8GFb-V6NWDKceDZ02VNi9kGxKAKP/view?usp=sharing&pli=1" target="_blank">here</a> for early viewing.',
   },
 ];
 </script>
 
 <style scoped lang="scss">
+* {
+  font-family: 'Poppins', sans-serif;
+}
+
 .container {
   margin-bottom: 5rem;
 }
@@ -134,10 +127,13 @@ template {
   margin-top: 5%;
 }
 
-.text-block h3,
-.text-block a {
-  color: $LIGHTGREEN;
+.text-block h3 {
+  color: white;
   text-align: center;
+}
+
+.text-block a {
+  color: #F1ADCD; 
 }
 
 .text-block {
@@ -150,7 +146,7 @@ template {
 
 .contact h3,
 .contact p {
-  color: $DARKPURPLE;
+  color: white;
 }
 
 .minor-graphic {
